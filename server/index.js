6a929def874b0ebe32b0a1cea9010a6f8fd60cf2
martysun25 <martysun25@gmail.com>
2017-08-24 11:47:10 +0800

    Add logger and npm script

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import path from 'path';
+import logger from 'morgan';
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../build/')));
 
+app.use(logger('common'));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../build/index.html'));
 })
 
-app.listen(process.env.PORT || 3001);
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log('server running on port ' + port);
+});
