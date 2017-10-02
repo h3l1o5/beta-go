6b0f34c931cdf11e74afcce96f1c7272887b12de
martysun25 <martysun25@gmail.com>
2017-10-02 18:14:20 +0800

    RealtimeDataRow: event and WDSD/POP

--- src/presentationBlock/realTimeDataRow/Event.js
+++ src/presentationBlock/realTimeDataRow/Event.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import _ from 'lodash'
 
 import './Event.css'
 
 const Event = props => (
   <div id="event" className="card">
     <div id="header">特殊狀況</div>
-    <div id="contents">{props.data.event}</div>
+    <div id="contents">
+      {_.isEmpty(props.data.event) ? (
+        <div id="tip">目前無特殊狀況</div>
+      ) : (
+        <div id="eventList">{props.data.event}</div>
+      )}
+    </div>
   </div>
 )
 
--- src/presentationBlock/realTimeDataRow/WindAndPOP.js
+++ src/presentationBlock/realTimeDataRow/WindAndPOP.js
@@ -2,13 +2,33 @@ import React from 'react'
 
 import './WindAndPOP.css'
 
-const Wind = props => (
-  <div id="windAndPOP" className="card">
-    <div id="header">風速與降雨機率</div>
-    <div id="contents">
-      {props.data.WDSD} {props.data.POP}
+const getTip = WDSD => {
+  switch (true) {
+    case WDSD >= 24.5:
+      return '路段封閉'
+    case WDSD >= 17.2:
+      return '速限降低至40km/h, 並持續禁止大型車行駛高架路段'
+    case WDSD >= 13.9:
+      return '速限降低至70km/h, 並禁止大型車行駛高架路段'
+    default:
+      return '依路段速限正常行駛'
+  }
+}
+
+const Wind = props => {
+  const tip = getTip(props.data.WDSD)
+  return (
+    <div id="windAndPOP" className="card">
+      <div id="header">風速與降雨機率</div>
+      <div id="contents">
+        <div id="WDSD">
+          <div id="content">風速 : {props.data.WDSD} 公尺每秒</div>
+          <div id="tip">{tip}</div>
+        </div>
+        <div id="POP">降雨機率 : {props.data.POP}%</div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Wind
